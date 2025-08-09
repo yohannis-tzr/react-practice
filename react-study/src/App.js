a3f2a3e18e0244c0ce713a5767109a9245b78128
yohannis-tzr <yotadzin@gmail.com>
2025-08-09 22:18:16 +0300

    roll

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
-      <Image></Image>
+      <img src={logo} className="App-logo" alt="logo" />
      
     </div>
   );
