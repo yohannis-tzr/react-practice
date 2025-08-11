e409a09ed2371ca95517ca8e36a30df063c6fe26
yohannis-tzr <yotadzin@gmail.com>
2025-08-11 23:56:41 +0300

    class

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/greet';
+import Welcome from './components/welcome';
 
 
 function App() {
   return (
     <div className="App">
       <Greet></Greet>
+      <Welcome></Welcome>
     </div>
   );
 }
---
+++ react-study/src/components/welcome.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Welcome extends Component {
+    render(){
+        return <h1>Welcome to React</h1>;
+    }
+
+}
+
+export default Welcome;
